87bfac153fb8f0d266418722e38cc99fd29bfe59
Victor Bergelin <victor.bergelin@gmail.com>
2016-06-28 07:45:27 -0700

    updates with opencv

--- detector.cpp
+++ detector.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-// #include "opencv2/opencv.hpp"
+#include "opencv2/opencv.hpp"
 
 using namespace std;
+
+int BUFSIZE = 7168;
 
 // WAVE PCM soundfile format (you can find more in https://ccrma.stanford.edu/courses/422/projects/WaveFormat/ )
 typedef struct header_file
@@ -26,24 +28,38 @@ typedef struct header_file
 
 typedef struct header_file* header_p;
 
-
 bool detect(short int buff16[])
 {
-	int length = (sizeof(buff16)/sizeof(*buff16));
-//	int i = 0;
+	int array_length = BUFSIZE;
 
-	for (int i = 0; i >= length; i++) 
-		cout << buff16[i] << endl;
+	// *** how to build filter opencv c++
+
+	// 1. transform buff16 to fft
+	// 2. filter fft
+	// 3. classify signal from condition
+	
+	float dummy_query_data[10] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	cv::Mat dummy_query = cv::Mat(2, 4, CV_32F, dummy_query_data);
+
+	cout << dummy_query.at<float>(0,2) << endl;
+	cout << dummy_query << endl;
+
+	// features:
+	// MAX VALUE:
+
+	cout << "-------------------------------------"  << endl;
+	int i = 0;
+
+	for (int i = 0; i <= array_length; i++) {
+		cout << i << ", " << buff16[i] << endl;
+	}
+	return 0;
 }
-
 
 
 int main()
 {
 	FILE * infile = fopen("audio.wav","rb");		// Open wave file in read mode
-
-	int BUFSIZE = 7168;
-
 	int count = 0;						// For counting number of frames in wave file.
 	short int buff16[BUFSIZE];				// short int used for 16 bit as input data format is 16 bit PCM audio
 	header_p meta = (header_p)malloc(sizeof(header));	// header_p points to a header struct that contains the wave file metadata fields
@@ -51,26 +67,24 @@ int main()
 	if (infile)
 	{
 		fread(meta, 1, sizeof(header), infile);
+
+		// READ Chunks of data
+		while (!feof(infile))
+		{
+			nb = fread(buff16,2,BUFSIZE,infile);		// Reading data in chunks of BUFSIZE
+			cout << nb << endl;
+			bool a;
+			count++;
+			a = detect(buff16);
+			if(a)
+			{
+				break;
+			}
+		}
+		
 		cout << " Size of Header file is "<<sizeof(*meta)<<" bytes" << endl;
 		cout << " Sampling rate of the input wave file is "<< meta->sample_rate <<" Hz" << endl;
 		cout << " Number of samples in wave file are " << meta->subchunk2_size << " samples" << endl;
-
-		// READ Chunks of data
-
-
-		while (!feof(infile))
-		{
-			nb = fread(buff16,1,BUFSIZE,infile);		// Reading data in chunks of BUFSIZE
-
-			// OPEN CV operations on each chunk
-			bool a;
-
-			//			cout << count <<endl;
-			count++;					// Incrementing Number of frames
-			/* Insert your processing code here*/
-			a = detect(buff16);
-		}
-		cout << sizeof(buff16) << endl;
 		cout << " Number of frames in the input wave file are " <<count << endl;
 	}
 	return 0;
