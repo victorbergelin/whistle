483d28796fe9d22338d91abf816e2dc466d2675a
Victor Bergelin <victor.bergelin@gmail.com>
2016-06-29 17:38:00 +0200

    new merged audio

---
+++ plotDftCsv.m
@@ -0,0 +1,21 @@
+
+main_path =  '/Users/victorbergelin/Dropbox/Repo/whistle/';
+cd(main_path);
+
+%%
+Array=csvread('d1.csv');
+hold on
+subplot(2,1,1)
+plot(Array)
+subplot(2,1,2)
+hist(Array,200)
+hold off
+
+
+%%
+
+[y1,Fs] = audioread('whistle1.wav');
+[y2,Fs] = audioread('whistle2.wav');
+[y3,Fs] = audioread('whistle3.wav');
+
+Y1 = fft(y1);
